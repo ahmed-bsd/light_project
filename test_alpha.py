9068cdd2b8e14cafb491d62c5d160c0138630e85
ahmed-bsd <81251782+ahmed-bsd@users.noreply.github.com>
2023-06-20 12:13:18 +0100

    alpha update

--- test_alpha.py
+++ test_alpha.py
@@ -1,4 +1,42 @@
- ######    letters detection   #############
+import pytesseract
+import cv2
+
+video_capture = cv2.VideoCapture(0) # 0 indicates the default camera
+
+config = '--psm 7' # Page segmentation mode for single character
+
+
+def filter_contours(contours):
+    min_width = 100
+    min_height = 100
+    max_width = 200
+    max_height = 200
+
+    filtered_contours = []
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        aspect_ratio = w / h
+        
+        if w > min_width and h > min_height and w < max_width and h < max_height and aspect_ratio > 0.2 and aspect_ratio < 1.8:
+            filtered_contours.append(contour)
+    
+    return filtered_contours
+
+def recognize_letters(characters):
+    recognized_letters = []
+    for character in characters:
+        recognized_letter = pytesseract.image_to_string(character, config=config)
+        recognized_letters.append(recognized_letter)
+    
+    return recognized_letters
+while True:
+    # Capture a frame from the camera feed
+    ret, frame = video_capture.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    ######    letters detection   #############
 
     # Apply thresholding to the gray
     _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
@@ -21,3 +59,14 @@
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame, recognized_letters[i], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    # Display the processed frame
+    cv2.imshow('Alphabetic Letters', frame)
+
+    # Quit the program when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close all windows
+video_capture.release()
+cv2.destroyAllWindows()
